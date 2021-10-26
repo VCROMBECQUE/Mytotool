daa859354c7092207282ea51f14f75961fc87b41
Valentin <valentin.crombecque@gmail.com>
2021-10-26 16:48:15 +0200

    update

--- todolist/php/connexion.php
+++ todolist/php/connexion.php
@@ -1,3 +1,27 @@
 <?php
 var_dump($_POST);
 
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$database  = 'mytotool';
+$charset = 'utf8';
+
+$dsn = "mysql:host=$servername;dbname=$database;charset=$charset";
+$conn = new PDO($dsn, $username, $password);
+$conn->beginTransaction();
+
+$sql = "SELECT users.* FROM users WHERE users.user LIKE :user OR users.email LIKE :user";
+$query = $conn->prepare($sql);
+$query->bindValue(":user", $_POST['identifiant'], PDO::PARAM_STR);
+
+if($query->execute()){
+    $conn->commit();
+}
+else{
+    $conn->rollBack();
+}
+
+$user_info = $query->fetchALL(PDO::FETCH_ASSOC);
+var_dump($user_info);
+
