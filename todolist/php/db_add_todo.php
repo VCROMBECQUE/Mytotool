51098bb92a9b7a79f0b4744929fcdcae664c49ed
Valentin <valentin.crombecque@gmail.com>
2021-11-15 11:24:53 +0100

    update

--- todolist/php/db_add_todo.php
+++ todolist/php/db_add_todo.php
@@ -17,11 +17,11 @@ if ($query_get_user_id->execute()) {
 }
 
 $conn->beginTransaction();
-$sql_add_todo = "INSERT INTO `todo` (`id`, `task`, `checked`, `user_id`) VALUES (NULL, :tache, '0', :user_id)";
+$sql_add_todo = "INSERT INTO `todo` (`id`, `task`, `user_id`) VALUES (NULL, :tache, :user_id)";
 
 $query_add_todo = $conn->prepare($sql_add_todo);
-$query_add_todo->bindValue(":tache", $input['tache'], PDO::PARAM_STR);
-$query_add_todo->bindValue(":user_id", $user_id['id'], PDO::PARAM_STR);
+$query_add_todo->bindValue(":tache", $input['tache']);
+$query_add_todo->bindValue(":user_id", $user_id['id']);
 if ($query_add_todo->execute()) {
     $conn->commit();
 } else {
